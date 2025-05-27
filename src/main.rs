599e8bbbaa683c7c00b393a94af9a879e8f72457
Aaryan Mishra <aaryan.mshra@gmail.com>
2025-05-27 23:30:46 +0530

    extended json string

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,32 @@ fn main() {
     // raw json string
     let json = r#"
     {
-        "name": "Alice",
-        "age": 30,
-        "height": 5.5,
-        "address": {
-            "city": "Wonderland",
-            "zip": 12345
+        "string": "hello \"world\" \\ / \b \f \n \r \t \u1234",
+        "number_integer": 123,
+        "number_negative": -456,
+        "number_float": 78.9,
+        "negative_number_float": -85.545,
+        "number_exp": 6.022e23,
+        "number_exp_neg": -1.6e-19,
+        "boolean_true": true,
+        "boolean_false": false,
+        "null_value": null,
+        "empty_object": {},
+        "empty_array": [],
+        "nested_array": [1, 2, [3, 4], {"nested": "object"}],
+        "nested_object": {
+            "level1": {
+                "level2": {
+                    "level3": "deep value"
+                }
+            }
         },
-        "is_student": false,
-        "extra": null
+        "array_of_objects": [
+            { "id": 1, "valid": true },
+            { "id": 2, "valid": false }
+        ],
+        "complex_escape": "This is a line\\nwith escapes\\tand unicode: \\u00A9",
+        "unicode_emoji": "😀 😁 😂 🤖 🦀"
     }
     "#;
 
